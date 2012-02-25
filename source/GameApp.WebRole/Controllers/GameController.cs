656bebba8ecb39c84a0f239b3b6ad06216824f00
Nathan Totten <ntotten@microsoft.com>
2012-02-25 14:34:37 -0500

    New Controllers

---
+++ source/GameApp.WebRole/Controllers/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameApp.WebRole.Controllers
+{
+    public class GameController : ApiController
+    {
+        // GET /api/game
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET /api/game/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST /api/game
+        public void Post(string value)
+        {
+        }
+
+        // PUT /api/game/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE /api/game/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ source/GameApp.WebRole/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameApp.WebRole.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET /api/user
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET /api/user/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST /api/user
+        public void Post(string value)
+        {
+        }
+
+        // PUT /api/user/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE /api/user/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
