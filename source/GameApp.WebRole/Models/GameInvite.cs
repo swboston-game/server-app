701a212df89e1334f154f71caf1dafbb75eb6700
Nathan Totten <ntotten@microsoft.com>
2012-02-25 16:08:30 -0500

    Updated Models

--- source/GameApp.WebRole/Models/GameInvite.cs
+++ source/GameApp.WebRole/Models/GameInvite.cs
@@ -7,6 +7,14 @@ namespace GameApp.WebRole.Models
 {
     public class GameInvite
     {
+
         public int Id { get; set; }
+        public DateTime InvitedOn { get; set; }
+        public DateTime? RespondedOn { get; set; }
+        public User Invitor { get; set; }
+        public User Invitee { get; set; }
+        public string InviteCode { get; set; }
+        public Game Game { get; set; }
+
     }
 }
---
+++ source/GameApp.WebRole/Models/GamePiece.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameApp.WebRole.Models {
+    public class GamePiece {
+
+        public int Id { get; set; }
+        public long FacebookId { get; set; }
+        public Game Game { get; set; }
+
+    }
+}
